5f8a3622df1d8d6241d894408a6475854b63cd6c
Kimberley1012 <31492515+Kimberley1012@users.noreply.github.com>
2018-06-10 18:06:38 -0500

    Add files via upload

---
+++ taxes3/models.py
@@ -0,0 +1,57 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+import random
+
+doc = """
+This is a one-period public goods game with 2 players.
+"""
+
+class Constants(BaseConstants):
+    name_in_url = 'taxes3'
+    players_per_group = 2
+    num_rounds = 2
+
+    instructions_template = 'taxes3/Instructions.html'
+
+    # """Amount allocated to each player"""
+    endowment = c(100)
+    multiplier = 1
+
+
+class Subsession(BaseSubsession):
+    def vars_for_admin_report(self):
+        ideclarado = [p.ideclarado for p in self.get_players() if p.ideclarado != None]
+        if ideclarado:
+            return {
+                'avg_ideclarado': sum(ideclarado)/len(ideclarado),
+                'min_ideclarado': min(ideclarado),
+                'max_ideclarado': max(ideclarado),
+            }
+        else:
+            return {
+                'avg_ideclarado': '(no data)',
+                'min_ideclarado': '(no data)',
+                'max_ideclarado': '(no data)',
+            }
+
+class Group(BaseGroup):
+    total_ideclarado = models.CurrencyField()
+
+    individual_share = models.CurrencyField()
+
+
+    def set_payoffs(self):
+        self.total_ideclarado = sum([0.28*p.ideclarado for p in self.get_players()])
+        self.individual_share = self.total_ideclarado * Constants.multiplier / Constants.players_per_group
+        for p in self.get_players():
+            p.payoff = (Constants.endowment - 0.28*p.ideclarado)
+
+
+class Player(BasePlayer):
+    ideclarado = models.CurrencyField(
+        min=0, max=100,
+        doc="""Monto declarado por el participante""",
+    )
+
---
+++ taxes3/views.py
@@ -0,0 +1,44 @@
+from . import models
+from ._builtin import Page, WaitPage
+from otree.api import Currency as c, currency_range
+from .models import Constants
+
+
+class Introduction(Page):
+    """Description of the game: How to play and returns expected"""
+    def is_displayed(self):
+        return self.round_number == 1
+
+class Contribute(Page):
+    """Player: Choose how much to contribute"""
+
+    form_model = models.Player
+    form_fields = ['ideclarado']
+
+    timeout_submission = {'ideclarado': c(Constants.endowment / 2)}
+
+
+class ResultsWaitPage(WaitPage):
+    def after_all_players_arrive(self):
+        self.group.set_payoffs()
+
+    body_text = "Esperando la decisión del otro participante"
+
+
+class Results(Page):
+    """Players payoff: How much each has earned"""
+
+    def vars_for_template(self):
+        return {
+            'total_earnings': self.group.total_ideclarado * Constants.multiplier,
+        }
+
+
+page_sequence = [
+    Introduction,
+    Contribute,
+    ResultsWaitPage,
+    Results
+]
+
+
